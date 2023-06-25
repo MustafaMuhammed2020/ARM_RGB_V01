5ffe2cf39cd3c4c9bf128fa7332345526a256b18
Mustafa Mohammed <mustafamohammed0321@gmail.com>
2023-06-25 19:02:15 +0300

    Last changes

--- Objects/main.d
+++ Objects/main.d
@@ -1,5 +1,1 @@
-./objects/main.o: main.c SERVICE\common_macros.h SERVICE\standard_types.h \
-  MCAL\GPIO\GPIO_interface.h MCAL\GPIO\GPIO_private.h \
-  HAL\BTN\button_interface.h HAL\BTN\button_config.h \
-  HAL\LED\LED_interface.h HAL\LED\..\..\SERVICE\standard_types.h \
-  HAL\LED\LED_config.h APP\APP.h
+./objects/main.o: main.c APP\APP.h
--- main.c
+++ main.c
@@ -5,30 +5,15 @@
 /* Hint    : implementation of APP APIs           */
 /**************************************************/
 
-/** INCLUDE LIBRARIES **/
-#include "SERVICE/common_macros.h"
-#include "SERVICE/standard_types.h"
-
-/** INTERFACE FILES OF LOWER LAYERS **/
-#include "MCAL/GPIO/GPIO_interface.h"
-#include "MCAL/GPIO/GPIO_private.h"
-#include "HAL/BTN/button_interface.h"
-#include "HAL/BTN/button_config.h"
-
-#include "HAL/LED/LED_interface.h"
-#include "HAL/LED/LED_config.h"
-
 #include "APP/APP.h"
-#include "HAL/LED/LED_interface.h"
 
-/** 0x40000000, 0x400FFFFF **/
+
 int main(void)
 {
 	
 	APP_init() ;
 	
-	
-	
+
 	while(1)
 	{
 		APP_start();
